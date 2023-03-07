d8240a4ce9a04e35bb378c1f8715b8dd5d790575
Matthias Theiss <matthias.theiss@wsa.com>
2023-03-07 08:53:43 +0100

    chore: only support macOS

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "StringCalculator",
+	platforms: [.macOS(.v10_13)],
     products: [
         .library(
             name: "StringCalculator",
